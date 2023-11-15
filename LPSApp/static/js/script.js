c5358b524d23faccc19ab18d3792f823967edeb0
KaitlynU <kaiturano@gmail.com>
2023-11-15 10:50:17 -0600

    Light/Dark Mode

--- LPSApp/static/js/script.js
+++ LPSApp/static/js/script.js
@@ -1,4 +1,4 @@
-// ---------------- Up Arrow ----------------
+// ---------------- Up Arrow -----------------
 document.addEventListener("DOMContentLoaded", function () {
     const scrollToTopButton = document.getElementById("scroll-to-top");
 
@@ -19,36 +19,42 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // // -----------------------------------------Dark Mode--------------------------
-// const icon = document.querySelector(".mode");
-// const switchInput = document.getElementById('switch');
+window.addEventListener('DOMContentLoaded', () => {
+    const icon = document.querySelector(".mode");
+    const switchInput = document.getElementById('switch');
+    
+    // Initialize color mode based on local storage or system preferences
+    const preferredColorMode = localStorage.getItem('colorMode');
+    if (preferredColorMode === 'dark-mode') {
+        applyColorPalette('dark-mode');
+        switchInput.checked = true;
+    } else {
+        applyColorPalette('scratchman');
+        switchInput.checked = false;
+    }
+    
+    icon.addEventListener("click", () => {
+        toggleColorPalette();
+    });
+});
 
-// // Initialize color mode based on local storage or system preferences
-// const preferredColorMode = localStorage.getItem('colorMode');
-// if (preferredColorMode === 'light-mode') {
-//     applyColorPalette('light-mode');
-//     switchInput.checked = true;
-// } else {
-//     applyColorPalette('dark-mode');
-//     switchInput.checked = false;
-// }
+function toggleColorPalette() {
+    const switchInput = document.getElementById('switch');
+    if (switchInput.checked) {
+        applyColorPalette('dark-mode');
+        localStorage.setItem('colorMode', 'dark-mode');
+    } else {
+        applyColorPalette('scratchman');
+        localStorage.setItem('colorMode', 'scratchman');
+    }
+}
 
-// icon.addEventListener("click", toggleColorPalette);
+function applyColorPalette(palette) {
+    const root = document.documentElement;
+    root.className = palette;
+}
 
-// function toggleColorPalette() {
-//     const switchInput = document.getElementById('switch');
-//     if (switchInput.checked) {
-//         applyColorPalette('light-mode');
-//         localStorage.setItem('colorMode', 'light-mode');
-//     } else {
-//         applyColorPalette('dark-mode');
-//         localStorage.setItem('colorMode', 'dark-mode');
-//     }
-// }
-
-// function applyColorPalette(palette) {
-//     const root = document.documentElement;
-//     root.className = palette;
-// }
+//--------------------------End Dark Mode----------------------------
 
 function toggleMenu(button){
     const content = document.querySelector('.dropdown-content');
